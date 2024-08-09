fdacc9b9d2d2c64e80d2d8a4bf85babe8e35668f
spriyansh <spriyansh29@gmail.com>
2024-08-09 12:37:47 +0200

    Initial Commit for data in package

---
+++ PCAdash/R/data_doc.R
@@ -0,0 +1,66 @@
+#' @title Named Pseudotime Vector
+#'
+#' @description
+#' A named vector of Pseudotime per cell computed using Monocle3.
+#'
+#' @usage
+#' # Loading
+#' data("pTime", package = "PCAdash")
+#'
+#' @author Priyansh Srivastava \email{spriyansh29@@gmail.com}
+#'
+"pTime"
+
+#' @title Named Cell Type Vector
+#'
+#' @description
+#' A named vector of Cell Type per cell Annotated using Azimuth.
+#'
+#' @usage
+#' # Loading
+#' data("cell_type", package = "PCAdash")
+#'
+#' @author Priyansh Srivastava \email{spriyansh29@@gmail.com}
+#'
+"cell_type"
+
+
+#' @title t-SNE Coordinates
+#'
+#' @description
+#' t-SNE Coordinates per cell in simple Matrix.
+#'
+#' @usage
+#' # Loading
+#' data("tsne_coords", package = "PCAdash")
+#'
+#' @author Priyansh Srivastava \email{spriyansh29@@gmail.com}
+#'
+"tsne_coords"
+
+
+#' @title Raw Counts
+#'
+#' @description
+#' Raw Expression Counts in a Simple Matrix.
+#'
+#' @usage
+#' # Loading
+#' data("counts", package = "PCAdash")
+#'
+#' @author Priyansh Srivastava \email{spriyansh29@@gmail.com}
+#'
+"counts"
+
+#' @title PCA Computations
+#'
+#' @description
+#' S3 of PCA Results Selected Metagenes for over 80% variance.
+#'
+#' @usage
+#' # Loading
+#' data("metagene_results", package = "PCAdash")
+#'
+#' @author Priyansh Srivastava \email{spriyansh29@@gmail.com}
+#'
+"metagene_results"
