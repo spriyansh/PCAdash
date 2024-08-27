b2b06110a094d0f9313b7a35142cec7348962ee8
spriyansh <spriyansh29@gmail.com>
2024-08-27 20:52:05 +0200

    Update Remotes

--- inst/deploy/shinyAppsIO.R
+++ inst/deploy/shinyAppsIO.R
@@ -18,7 +18,7 @@ rsconnect::setAccountInfo(
 
 ## Install PCAdash package from GitHub using auth_token
 Sys.setenv(GITHUB_PAT = Sys.getenv("PUBLIC_INSTALL_GIT_PAT"));
-gitcreds_set(url = "https://github.com", password = Sys.getenv("PUBLIC_INSTALL_GIT_PAT"))
+gitcreds::gitcreds_set(url = "https://github.com", Sys.getenv("PUBLIC_INSTALL_GIT_PAT"))
 remotes::install_github(
     repo       = "spriyansh/PCAdash",
     ref        = "main")
