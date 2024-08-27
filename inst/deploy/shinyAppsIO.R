cefb9c8a1849044453845e30faa2f1eae1ec49d0
spriyansh <spriyansh29@gmail.com>
2024-08-27 18:51:57 +0200

    Update Remotes

--- inst/deploy/shinyAppsIO.R
+++ inst/deploy/shinyAppsIO.R
@@ -1,15 +1,17 @@
 ## Install packages
-install.packages(c("rsconnect", "remotes"),
+install.packages(c("rsconnect", "remotes", "gitcreds"),
                  repos = "https://cloud.r-project.org/")
 
 ## Load Rsconnect
 suppressPackageStartupMessages({
   library(rsconnect)
   library(remotes)
+  library(gitcreds)
 })
 
 ## Set PAT
-Sys.setenv(GITHUB_PAT = Sys.getenv("PUBLIC_INSTALL_GIT_PAT"))
+pat <- Sys.getenv("PUBLIC_INSTALL_GIT_PAT")
+gitcreds::gitcreds_set(pat)
 
 ## Set keys
 rsconnect::setAccountInfo(
